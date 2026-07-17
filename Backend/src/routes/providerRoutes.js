1f91fc25652d6e446705bac86e7abf8282066ab4
Sanjog Sharma <sharmasanjog2005@gmail.com>
2026-07-17 23:26:31 +0545

    Restore previous backend before clean re-add

--- Backend/src/routes/providerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { apply } = require('../controllers/providerController');
-const requireAuth = require('../middleware/requireAuth');
-
-const router = express.Router();
-router.post('/apply', requireAuth, apply);
-
-module.exports = router;
